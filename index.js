e0e8fbf3f72d5399ca04a027d89c49725e00e224
LakeCombs <adesolaolamilekan2@gmail.com>
2021-10-24 15:02:19 +0100

    parcer introduction

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = () => <div>Hello what are we doing ?</div>;
+
+ReactDOM.render(<Hello />, document.getElementById("app"));
